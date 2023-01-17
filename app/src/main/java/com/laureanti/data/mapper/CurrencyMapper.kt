37df26e9fbb44c46fe8a2df8d4d25ecfb5c23f56
Daniil Laureanti <daniil.laureanti@gmail.com>
2023-01-17 11:58:44 +0100

    Add currency mapper

--- app/src/main/java/com/laureanti/data/mapper/CurrencyMapper.kt
+++ app/src/main/java/com/laureanti/data/mapper/CurrencyMapper.kt
@@ -1,20 +1,43 @@
 package com.laureanti.data.mapper
 
+import com.laureanti.data.network.model.ConvertCurrencyDto
 import com.laureanti.data.network.model.CurrencyDto
+import com.laureanti.domain.entity.ConvertCurrency
+import com.laureanti.domain.entity.Currency
 
 class CurrencyMapper {
 
-    fun mapJsonRatesToMapCurrencyRates(jsonRates: CurrencyDto): Map<String, Double>? {
-        val mapCurrencyRates = mutableMapOf<String, Double>()
+    fun mapDtoToEntity(convertCurrencyDto: ConvertCurrencyDto) = convertCurrencyDto.result?.let {
+        ConvertCurrency(
+            result = it
+        )
+    }
+
+    fun mapJsonRatesToListCurrency(jsonRates: CurrencyDto): List<Currency> {
+        val listCurrencyNames = mapJsonRatesToListCurrencyNames(jsonRates)
+        val listCurrencies = listCurrencyNames?.let { mapListCurrencyNamesToListCurrency(it) }
+        return listCurrencies ?: throw RuntimeException("Api return null")
+    }
+
+    private fun mapJsonRatesToListCurrencyNames(jsonRates: CurrencyDto): List<String>? {
+        val currencyNames = mutableListOf<String>()
 
         val jsonObject = jsonRates.jsonRates ?: return null
         val currencyKeySet = jsonObject.keySet()
 
         for (currencyKey in currencyKeySet) {
-            mapCurrencyRates[currencyKey] = jsonObject.get(currencyKey).asDouble
+            currencyNames.add(currencyKey)
 
         }
-        return mapCurrencyRates
+        return currencyNames
+    }
+
+    private fun mapListCurrencyNamesToListCurrency(listCurrencyNames: List<String>): List<Currency> {
+        val listCurrency = mutableListOf<Currency>()
+        for (currencyName in listCurrencyNames) {
+            listCurrency.add(Currency(currencyName))
+        }
+        return listCurrency
     }
 
 }
